b4cd1e6b93dc1fe3a5a9f444f54eca94428ba351
adrientermini <adrien.termini@student.hepl.be>
2022-10-20 01:45:15 +0200

    :zap: Resolving CORS

--- src/main/java/com/train/todoapp/controller/TodoController.java
+++ src/main/java/com/train/todoapp/controller/TodoController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Getter
 @RestController
 @RequestMapping("/todo")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class TodoController {
 
